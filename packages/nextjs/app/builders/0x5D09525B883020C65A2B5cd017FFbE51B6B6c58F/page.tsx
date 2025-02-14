0c6164e75d4379ad16c1b0704b15f48ea0816db5
xander.nesta <xandev.eth@protonmail.com>
2025-02-14 15:03:52 -0500

    remove React type import, make text more of a highlight for readability, add mapping for IconLinks

--- packages/nextjs/app/builders/0x5D09525B883020C65A2B5cd017FFbE51B6B6c58F/page.tsx
+++ packages/nextjs/app/builders/0x5D09525B883020C65A2B5cd017FFbE51B6B6c58F/page.tsx
@@ -12,7 +12,29 @@ const inter = Inter({
   fallback: ["ui-sans-serif"],
 });
 
-const Profile: NextPage = () => {
+const XanDevProfile: NextPage = () => {
+  const socialLinks = [
+    { href: "https://github.com/xandev3", icon: <FaGithub size={20} />, label: "GitHub" },
+    { href: "https://t.me/XanDev3", icon: <FaTelegram size={20} />, label: "Telegram: @XanDev3" },
+    { href: "mailto:xandev.eth@gmail.com", icon: <FaEnvelopeSquare size={22} />, label: "Email: xandev.eth@gmail.com" },
+  ];
+
+  interface IconLinkProps {
+    href: string;
+    icon: React.ReactNode;
+    label: string;
+  }
+  
+  const IconLink: React.FC<IconLinkProps> = ({ href, icon, label }) => {
+    return (
+      <li>
+        <Link href={href} className="flex items-center space-x-3 link no-underline">
+          {icon}
+          <span>{label}</span>
+        </Link>
+      </li>
+    );
+  };
   return (
     <div className={`${inter.variable} font-serif col-span-1 grid grid-cols-1 m-auto justify-center`}>
       <div className="grid grid-cols-1 px-6 w-full max-w-4xl my-0 ">
@@ -32,28 +54,18 @@ const Profile: NextPage = () => {
           </div>
           <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-6 lg:px-8 mb-6 space-y-3 py-4  sm:p-6 ">
             <h2 className="text-2xl font-semibold mb-2">About Me</h2>
-            <p className="mb-8">
+            <p className="text-content text-justify mb-8">
               Hello, I go by Xander or XanDev. I&apos;m a passionate about learning to be a software developer and web3
-              engineer, currently working on building Dapps and working on opensource projects. I started my career as
-              Systems Administrator for 5 years supporting Windows and Linux environments and decided to make a career
-              change to self-learn software development.
-              <br />
-              <br />
-              I began with the Javascript/NodeJs Odin Project course and then completed Cyfrin&apos;s Solidity Smart
-              Contract Development and now working on SpeedRunEthereum. I love learning about all the new and exciting
-              developments on Ethereum and in the blockchain space. I hope to be able to start contributing to more
-              Ethereum projects soon!
+              engineer, currently working on building Dapps and opensource projects. I started my career as a SRE for 5
+              years and decided to make a career change to self-learn software development for web2 and web3. I love
+              learning about all the new and exciting developments on Ethereum and in the blockchain space.
               <br />
             </p>
             <h2 className="text-2xl font-semibold">Connect with Me</h2>
             <ul className="space-y-3">
-              <IconLink href="https://github.com/xandev3" icon={<FaGithub size={20} />} label="GitHub" />
-              <IconLink href="https://t.me/XanDev3" icon={<FaTelegram size={20} />} label="Telegram: @XanDev3" />
-              <IconLink
-                href="mailto:xandev.eth@gmail.com"
-                icon={<FaEnvelopeSquare size={22} />}
-                label="Email: xandev.eth@gmail.com"
-              />
+              {socialLinks.map(({ href, icon, label }) => (
+                <IconLink key={href} href={href} icon={icon} label={label} />
+              ))}
             </ul>
           </div>
         </div>
@@ -62,21 +74,4 @@ const Profile: NextPage = () => {
   );
 };
 
-interface IconLinkProps {
-  href: string;
-  icon: React.ReactNode;
-  label: string;
-}
-
-const IconLink: React.FC<IconLinkProps> = ({ href, icon, label }) => {
-  return (
-    <li>
-      <Link href={href} className="flex items-center space-x-3 link no-underline">
-        {icon}
-        <span>{label}</span>
-      </Link>
-    </li>
-  );
-};
-
-export default Profile;
+export default XanDevProfile;
