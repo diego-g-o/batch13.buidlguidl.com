cf93ae8276c30868f459282717759956885b0dcc
divyansh <dewangandivyansh799@gmail.com>
2025-02-19 00:50:05 +0530

    Removed SwitchTheme, Added <Image> and <Link>

--- packages/nextjs/app/builders/0x6C0FB309933931d111634921287aF902667032C4/page.tsx
+++ packages/nextjs/app/builders/0x6C0FB309933931d111634921287aF902667032C4/page.tsx
@@ -1,24 +1,10 @@
-"use client";
+import Image from "next/image";
+import Link from "next/link";
 
-import SwitchTheme from "./SwitchTheme";
-import { useTheme } from "next-themes";
-
-const NewPage = () => {
-  const { resolvedTheme } = useTheme();
-
+const DivyanshPage = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-500">
-      <div
-        className="w-[600px] p-6 rounded-lg shadow-lg transition-all duration-500"
-        style={{
-          background:
-            resolvedTheme === "dark"
-              ? "linear-gradient(135deg, #8B0000, #000000)"
-              : "linear-gradient(135deg, #FF6347, #FFA500)",
-          color: resolvedTheme === "dark" ? "#ffffff" : "#000000",
-        }}
-      >
-        <SwitchTheme />
+      <div className="w-[600px] p-6 rounded-lg shadow-lg transition-all duration-500 bg-gradient-to-br from-red-500 to-orange-400 dark:from-red-900 dark:to-black text-black dark:text-white">
         <Avatar />
         <NameandBio />
         <Socials />
@@ -30,10 +16,12 @@ const NewPage = () => {
 function Avatar() {
   return (
     <div className="flex items-center">
-      <img
+      <Image
         src="https://i.pinimg.com/474x/d7/31/4c/d7314c015805cbf159e1099c869233ee.jpg"
         alt="Avatar"
-        className="w-24 h-24 rounded-full mr-4 border-2 border-white"
+        width={96}
+        height={96}
+        className="rounded-full mr-4 border-2 border-white"
       />
       <p className="text-lg font-semibold">0x6C0FB309933931d111634921287aF902667032C4</p>
     </div>
@@ -57,15 +45,15 @@ function Socials() {
     <div className="text-center mt-6">
       <p className="text-lg font-semibold">Connect with me:</p>
       <div className="flex justify-center space-x-6 mt-2">
-        <a href="https://x.com/DivyanshD_7" target="_blank" className="text-blue-400 hover:underline">
+        <Link href="https://x.com/DivyanshD_7" target="_blank" className="text-blue-400 hover:underline">
           X
-        </a>
-        <a href="https://www.instagram.com/divyanshh._.20" target="_blank" className="text-pink-500 hover:underline">
+        </Link>
+        <Link href="https://www.instagram.com/divyanshh._.20" target="_blank" className="text-pink-500 hover:underline">
           Instagram
-        </a>
+        </Link>
       </div>
     </div>
   );
 }
 
-export default NewPage;
+export default DivyanshPage;
