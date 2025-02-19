6d7a7465e614b718f6a7108170a6b3380c35904d
divyansh <dewangandivyansh799@gmail.com>
2025-02-19 19:43:48 +0530

    Fixed rendering issues, changed colors of socials, added NextPage

--- packages/nextjs/app/builders/0x6C0FB309933931d111634921287aF902667032C4/page.tsx
+++ packages/nextjs/app/builders/0x6C0FB309933931d111634921287aF902667032C4/page.tsx
@@ -1,6 +1,6 @@
-import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { NextPage } from "next";
 
 const DivyanshPage: NextPage = () => {
   return (
