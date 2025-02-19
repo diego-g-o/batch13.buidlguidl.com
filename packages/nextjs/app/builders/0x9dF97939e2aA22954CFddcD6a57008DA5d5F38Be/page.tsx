9cf26c34b30225fc344700553af4c5703ce6a39a
Strapchay <ayanelaw@outlook.com>
2025-02-19 20:57:37 +0100

    modified page to reflect review

--- packages/nextjs/app/builders/0x9dF97939e2aA22954CFddcD6a57008DA5d5F38Be/page.tsx
+++ packages/nextjs/app/builders/0x9dF97939e2aA22954CFddcD6a57008DA5d5F38Be/page.tsx
@@ -4,6 +4,21 @@ import type { NextPage } from "next";
 import { FaGithub } from "react-icons/fa";
 import { FaTelegram, FaXTwitter } from "react-icons/fa6";
 
+const IconObjects = [
+  {
+    link: "https://github.com/strapchay",
+    icon: FaGithub,
+  },
+  {
+    link: "https://x.com/strapchay",
+    icon: FaXTwitter,
+  },
+  {
+    link: "https://t.me/yahcparts",
+    icon: FaTelegram,
+  },
+];
+
 const StrapchayProfile: NextPage = () => {
   return (
     <>
@@ -13,21 +28,17 @@ const StrapchayProfile: NextPage = () => {
             <Image className="w-full h-full" src="/punkvibes.png" alt="Profile picture" width={500} height={500} />
           </div>
           <div className="w-8 flex flex-col justify-evenly">
-            <Link href="https://github.com/strapchay">
-              <FaGithub size={30} />
-            </Link>
-            <Link href="https://x.com/strapchay">
-              <FaXTwitter size={30} />
-            </Link>
-            <Link href="https://t.me/yahcparts">
-              <FaTelegram size={30} />
-            </Link>
+            {IconObjects.map(({ link, icon: Icon }, i) => (
+              <Link key={i} href={link ?? ""}>
+                <Icon size={30} />
+              </Link>
+            ))}
           </div>
         </section>
 
         <section
           id="title"
-          className="gap-[2px] flex items-center flex-col bg-stone-200 dark:bg-zinc-500 rounded px-4 py-2 dark:text-slate-200 text-zinc-500"
+          className="gap-[2px] flex items-center flex-col bg-[#1B1725] dark:bg-zinc-500 rounded px-4 py-2 dark:text-slate-200 text-zinc-100"
         >
           <h3 className="font-bold">Strapchay</h3>
           <p className="m-0 text-xs italic lowercase">Full-Stack Web Dev &amp; Web3 Developer</p>
@@ -35,7 +46,7 @@ const StrapchayProfile: NextPage = () => {
 
         <section
           id="details"
-          className="w-[50%] max-sm:w-[65%] justify-center text-justify mt-4 dark:bg-slate-500 rounded-xl px-4 py-2 dark:text-sky-950 bg-zinc-400 text-sl"
+          className="w-[50%] max-sm:w-[65%] justify-center text-justify mt-4 dark:bg-slate-500 rounded-xl px-4 py-2 dark:text-sky-950 bg-[#4b3b47] text-sl text-white"
         >
           <p className="m-0">
             Hi all! I&apos;m a full-stack web developer with 3+ years experience. My programming languages are Python,
