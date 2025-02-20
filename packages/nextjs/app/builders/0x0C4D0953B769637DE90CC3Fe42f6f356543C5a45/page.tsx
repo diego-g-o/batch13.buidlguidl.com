da1708f9874f8d0d21ad9b2f4b2dcaddfa55194d
Nick Kasten <kastencode@gmail.com>
2025-02-20 15:47:42 -0600

    address PR feedback - rename page component - update spacing/layout - use SE2 Address component - add HTML5 address component

--- packages/nextjs/app/builders/0x0C4D0953B769637DE90CC3Fe42f6f356543C5a45/page.tsx
+++ packages/nextjs/app/builders/0x0C4D0953B769637DE90CC3Fe42f6f356543C5a45/page.tsx
@@ -1,27 +1,32 @@
 import Image from "next/image";
 import { NextPage } from "next";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Address } from "~~/components/scaffold-eth";
 
-const BioPage: NextPage = () => {
+const NickKastenBuilderPage: NextPage = () => {
   return (
     <div className="max-w-2xl mx-auto p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
-      <div className="flex items-center mb-6">
+      <div className="flex items-center mt-6 mb-6">
         <Image src="/ntk.webp" alt="Nick Kasten" width={128} height={128} className="rounded-full mr-4" />
         <h1 className="text-5xl font-bold text-gray-800 dark:text-white">Nick Kasten</h1>
       </div>
 
       <div className="mb-6">
-        <pre className="font-semibold">0x0c4d0953b769637de90cc3fe42f6f356543c5a45</pre>
+        <Address address="0x0c4d0953b769637de90cc3fe42f6f356543c5a45" format="long" />
         <p className="text-gray-600 dark:text-gray-300 mb-4">
           Full-stack software engineer with 5 years of experience in Python and JavaScript/TypeScript. Passionate about
           web3 development and contributing to open-source!
         </p>
       </div>
 
-      <div className="mb-4">
+      <div className="mb-6">
         <h2 className="text-xl font-semibold text-gray-700 dark:text-gray-200">Contact</h2>
-        <p className="text-gray-600 dark:text-gray-300">📍 Austin TX, USA</p>
-        <p className="text-gray-600 dark:text-gray-300">📧 kastencode@gmail.com</p>
+        <address className="not-italic">
+          <p className="text-gray-600 dark:text-gray-300">📍 Austin TX, USA</p>
+          <a href="mailto:kastencode@gmail.com" className="text-gray-600 dark:text-gray-300">
+            📧 kastencode@gmail.com
+          </a>
+        </address>
       </div>
 
       <div className="flex space-x-4">
@@ -46,4 +51,4 @@ const BioPage: NextPage = () => {
   );
 };
 
-export default BioPage;
+export default NickKastenBuilderPage;
